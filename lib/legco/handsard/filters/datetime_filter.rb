27093423fbf54eb30c9f50bc407624ab93046493
Francis Chong <francis@ignition.hk>
2013-05-18 18:08:06 +0800

    date time filter now parse time

--- lib/legco/handsard/filters/datetime_filter.rb
+++ lib/legco/handsard/filters/datetime_filter.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'pry'
 
 module Legco
   module Handsard
@@ -6,10 +7,37 @@ module Legco
       # Extract Date and Time of the meeting
       class DatetimeFilter
         def process(doc)
-          date_matches = doc.text.match %r{\n會議過程正式紀錄\s*\n([0-9]{4})年([0-9]{2})月([0-9]{2})日星期.\s*\n}
+          date_matches = doc.text.match %r{會議過程正式紀錄\s*\n([0-9]{4})年([0-9]{2})月([0-9]{2})日星期.}
           if date_matches
-            doc.date = Date.new(date_matches[1].to_i, date_matches[2].to_i, date_matches[3].to_i)
+            year = date_matches[1].to_i
+            month = date_matches[2].to_i
+            date = date_matches[3].to_i
+          else
+            raise "date not found"
           end
+
+          time_matches = doc.text.match %r{(.午)(.+)時(正|(.*分))會議開始}
+          if time_matches
+            apm = time_matches[1]
+            hour = time_matches[2]
+            minute = time_matches[3]
+
+            hour = hour.to_i
+
+            if minute == "正"
+              minute = 0
+            else
+              minute = minute.to_i
+            end
+
+            if apm == "下午"
+              hour += 12
+            end
+          else
+            raise "time not found"
+          end
+
+          doc.date = DateTime.new(year, month, date, hour, minute, 0)
         end
       end
     end
--- spec/filters/datetime_filter_spec.rb
+++ spec/filters/datetime_filter_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Legco::Handsard::Filters::DatetimeFilter do
-  let(:doc) {test_doc}
+  let(:doc) {
+    Legco::Handsard::Document.new <<-EOF
+會議過程正式紀錄 
+ 
+2012年11月21日星期三 
+ 
+上午11時正會議開始 
+ 
+ 
+出席議員： 
+EOF
+  }
 
   before do
     subject.process doc
@@ -9,7 +20,7 @@ describe Legco::Handsard::Filters::DatetimeFilter do
 
   describe "-process" do
     it "should extract date" do
-      doc.date.strftime("%Y%m%d").should == "20121121"
+      doc.date.strftime("%Y/%m/%d %H:%M").should == "2012/11/21 11:00"
     end
   end
 end
