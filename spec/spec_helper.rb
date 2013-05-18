0e9341c428864a594a3e92fbd29d3a0fca89ee08
Francis Chong <francis@ignition.hk>
2013-05-18 14:11:50 +0800

    add fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'legco'
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr'
+  c.cassette_library_dir = 'spec/fixtures/vcr'
   c.hook_into :webmock
   c.default_cassette_options = { :record => :new_episodes }
 end
