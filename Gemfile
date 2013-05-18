c526dbc2a7d1073795049507a617f236fe4ffec3
Francis Chong <francis@ignition.hk>
2013-05-18 13:58:29 +0800

    add vcd and gems

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source "https://rubygems.org"
 
 gem 'pdf-reader'
+gem 'nokogiri'
+
+gem 'vcr'
+gem 'webmock', '~> 1.9.0'
+
+gem 'pry'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH << "lib"
+require 'legco'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :new_episodes }
+end
+
+RSpec.configure do |c|
+  c.extend VCR::RSpec::Macros
+end
