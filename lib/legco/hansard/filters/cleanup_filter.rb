c120a16bcc4ddec18f26aeb9f600c6fbc98225cc
Francis Chong <francis@ignition.hk>
2013-05-18 21:28:54 +0800

    cleanup text for new text converter

--- lib/legco/hansard/filters/cleanup_filter.rb
+++ lib/legco/hansard/filters/cleanup_filter.rb
@@ -2,7 +2,7 @@ module Legco
   module Hansard
     module Filters
       class CleanupFilter
-        HEADER_REGEX = %r{立法會 ─ [0-9]{4}年[0-9]{1,2}月[0-9]{1,2}日\s\n\s*[0-9]+}
+        HEADER_REGEX = %r{(\A|\n)立法會\s*─\s*[0-9]{4}年\s*[0-9]{1,2}月\s*[0-9]{1,2}日\s*\n\s*[A0-9]+}
 
         def process(document)
           document.text.gsub! HEADER_REGEX, ''
--- spec/filters/cleanup_filter_spec.rb
+++ spec/filters/cleanup_filter_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 describe Legco::Hansard::Filters::CleanupFilter do
   describe "-process" do
     it "should remove headers" do
-      doc  = test_doc
+      doc  = Legco::Hansard::Document.new <<-EOF
+立法會  ─  2012年 11月 21日  
+ 
+1495
+EOF
       subject.process doc
-      doc.text.should_not be_include("立法會 ─ 2012年11月21日 \n1495 ")
-      doc.text.should_not be_include("立法會 ─ 2012年11月21日 \n1496 ")
+      doc.text.should_not be_include("立法會  ─  2012年 11月 21日")
+      doc.text.should_not be_include("1495")
     end
   end
 end
