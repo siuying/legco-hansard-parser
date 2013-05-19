08a139acbbe4b88c659bb350e4697399aa8eee5d
Francis Chong <francis@ignition.hk>
2013-05-19 12:49:13 +0800

    SpeechFilter also extract actions

--- lib/legco/hansard/filters/speech_filter.rb
+++ lib/legco/hansard/filters/speech_filter.rb
@@ -2,22 +2,49 @@ require 'state_machine'
 module Legco
   module Hansard
     module Filters
+      # extract speeches from hansard
       class SpeechFilter
         attr_reader :speeches
 
-        SPEECH_REGEX = %r{^[0-9]*\.?((.*)(主席|議員|局長|司長))：(.+)}
+        SPEECH_REGEX = %r{^[0-9]*\.?((.*)(代理主席|行政長官|主席|議員|局長|司長))：(.+)}
+        ACT_BEGIN_REGEX = %r{^\(([^\)]+)\)?\s*$}
+        ACT_END_REGEX = %r{([^\)]+)\)\s*$}
 
         state_machine :state, :initial => :header do
           event :speak do
             transition all => :speaking
+          end
+          
+          event :act do
+            transition :speaking => :acting
           end
 
           event :supplement do
             transition all => :supplementing
           end
 
+          after_transition :on => :speak, :do => :flush
+
           state :header do
             def process_line(line)
+            end
+          end
+
+          state :acting do
+            def process_line(line)
+              match = line.match(ACT_BEGIN_REGEX)
+              if match
+                @actions << match[1]
+                return
+              end
+
+              match = line.match(ACT_END_REGEX)
+              if match
+                @actions << match[1]
+                return
+              end
+
+              @actions << line
             end
           end
 
@@ -58,6 +85,7 @@ module Legco
           @current_text = []
           @speeches = []
           @current_supplement = []
+          @actions = []
           super()
         end
 
@@ -77,20 +105,35 @@ module Legco
             supplement
           elsif line.match %r{^主席：}
             speak
+          elsif line.match %r{^\([^\)]+\)?\s*$}
+            if speaking?
+              act
+            end
+          elsif line.match %r{\)\s*$}
+            # end of act
           elsif line.match %r{^(議員議案|香港特別行政區行政長官發表施政報告)}
+            # context
           end
         end
 
         def flush
           if @current_text.size > 0 && @current_speaker
             @speeches << {
+              :type => :speech,
               :speaker => @current_speaker, 
               :text => @current_text.join("\n"),
-              :supplement => @current_supplement.size > 0 ? @current_supplement.join("\n") : nil
+              :supplement => @current_supplement.size > 0 ? @current_supplement.join("\n").strip : nil
             }
             @current_supplement = []
             @current_text = []
-            @current_speaker = nil
+          end
+
+          if @actions.size > 0
+            @speeches << {
+              :type => :action,
+              :text => @actions.join("\n").strip
+            }
+            @actions = []
           end
         end
       end
--- spec/filters/speech_filter_spec.rb
+++ spec/filters/speech_filter_spec.rb
@@ -10,38 +10,54 @@ describe Legco::Hansard::Filters::SpeechFilter do
       subject.process doc
     end
 
-    it "should extract speaker, text and supplement" do
+    it "should extract speech" do
       doc.data[:speeches].should be_a(Array)
       doc.data[:speeches].should_not be_empty
 
-      speech = doc.data[:speeches].first
+      speeches = doc.data[:speeches].select {|sp| sp[:type] == :speech}
+
+      speech = speeches.first
       speech[:speaker][:title].should == "主席"
       speech[:speaker][:name].should == nil
       speech[:speaker][:full].should == "主席"
       speech[:text].should be_include("秘書，請響鐘傳召議員進入會議廳。")
       speech[:supplement].should be_include("下列文件是根據《議事規則》第21(2)條")
 
-      speech = doc.data[:speeches][1]
+      speech = speeches[1]
       speech[:speaker][:title].should == "主席"
       speech[:speaker][:name].should == nil
       speech[:speaker][:full].should == "主席"
       speech[:text].should be_include("質詢。第一項質詢。")
       speech[:supplement].should be_nil
 
-      speech = doc.data[:speeches][2]
+      speech = speeches[2]
       speech[:speaker][:title].should == "議員"
       speech[:speaker][:name].should == "林健鋒"
       speech[:speaker][:full].should == "林健鋒議員"
       speech[:text].should be_include("據報，法定最低工資的實施改變了勞動市場的薪酬")
       speech[:text].should be_include("若有調查，物價的升幅為")
 
-      speech = doc.data[:speeches][3]
+      speech = speeches[3]
       speech[:speaker][:title].should == "勞工及福利局局長"
       speech[:speaker][:name].should == nil
       speech[:speaker][:full].should == "勞工及福利局局長"
       speech[:text].should be_include("主席，法定最低工資實施以來，政府一直密切注")
       speech[:text].should be_include("定最低工資並不是去年通脹急升的最主要原因。")
       speech[:supplement].should be_include("2011年按行業劃分的總薪酬開支增加率")
+    end
+
+    it "should extract action" do
+      doc.data[:speeches].should be_a(Array)
+      doc.data[:speeches].should_not be_empty
+
+      speeches = doc.data[:speeches]
+
+      speech = speeches.first
+      speech[:type].should == :speech
+
+      speech = speeches[1]
+      speech[:type].should == :action
+      speech[:text].should == "在傳召鐘響後，多位議員進入會議廳"
     end
   end
 end
