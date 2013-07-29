ca4318597c60b95973a8c08269f2fd03661b5fac
Francis Chong <francis@ignition.hk>
2013-07-30 00:12:41 +0800

    Add back PDFBox binaries such that people can run it as is

--- lib/legco/hansard/pdf_converter.rb
+++ lib/legco/hansard/pdf_converter.rb
@@ -1,7 +1,8 @@
 module Legco
   module Hansard
     class PdfConverter
-      JAR_PATH = File.join(File.dirname(__FILE__), "../../../jar/pdfbox-app-1.8.1.jar")
+      JAR_VERSION = "1.8.2"
+      JAR_PATH = File.join(File.dirname(__FILE__), "../../../jar/pdfbox-app-#{JAR_VERSION}.jar")
 
       def convert(pdf_filename, txt_filename)
         system("java -jar #{JAR_PATH} ExtractText -encoding \"UTF-8\" \"#{pdf_filename}\" #{txt_filename}")
